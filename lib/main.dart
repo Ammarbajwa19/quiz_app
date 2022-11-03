d8aa5229de3eae1a8e8a810b6367ff1bcd828349
Ammarbajwa19 <ammarbajwah@gmail.com>
2022-11-03 05:29:49 +0500

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,38 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(myapp());
-}
+// void main() {
+//   runApp(myapp());
+// }
+void main() => runApp(myapp());
 
 class myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var question = {
+      "what is your favourite color ",
+      "what is your favourite animal",
+    };
     return MaterialApp(
-      home: Container(child: Text("Welcome herr is Ammar for your help")),
-    );
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("Ammar"),
+            ),
+            body: Column(children: [
+              Text("HI question!"),
+              
+              RaisedButton(
+                child: Text("Answer 1"),
+                onpressed: null), 
+                RaisedButton(
+                child: Text("Answer 2"),
+                onpressed: null), 
+                RaisedButton(
+                child: Text("Answer 3"),
+                onpressed: null,
+              ),
+            ])));
   }
+}
+
+RaisedButton({required Text child, required onpressed}) {
 }
